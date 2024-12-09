847a617a1e1c85b5831699c84b6d34914866adb1
Krzysztof Ruta <krzysztof.ruta@play.pl>
2024-12-09 22:17:17 +0100

    extract columns from schema for a simple type

--- pyspark_explorer/data_table.py
+++ pyspark_explorer/data_table.py
@@ -3,11 +3,18 @@ from pyspark.sql.types import StructField, Row
 
 class DataTable:
     def __init__(self, schema: [StructField], data: [Row]):
-        self._schema = schema
-        self._data = data
+        self._schema: [StructField] = schema
+        self._data: [Row] = data
 
-        self.columns = [{"index": 0, "name": "text", "type": "StringType", "field_type": schema[0]}]
+        self.columns = self.__extract_columns__()
         self.rows = [
-            {"index": 0, "column": self.columns[0], "value": "some text 1", "display_value": "some text 1"},
-            {"index": 1, "column": self.columns[0], "value": "some text 2", "display_value": "some text 2"},
+            {"row_index": 0, "column": self.columns[0], "value": "some text 1", "display_value": "some text 1"},
+            {"row_index": 1, "column": self.columns[0], "value": "some text 2", "display_value": "some text 2"},
         ]
+
+    def __extract_columns__(self) -> []:
+        cols = []
+        for i,field in enumerate(self._schema):
+            cols.append({"col_index": i, "name": field.name, "type": type(field.dataType).__name__, "field_type": field.dataType})
+
+        return cols
--- tests/tests.py
+++ tests/tests.py
@@ -13,11 +13,11 @@ class TestDataTable:
 
         tab = DataTable(schema, row)
 
-        expected_cols = [{"index": 0, "name": "text", "type": "StringType", "field_type": schema[0]}]
+        expected_cols = [{"col_index": 0, "name": "text", "type": "StringType", "field_type": schema[0].dataType}]
         assert tab.columns == expected_cols
         expected_rows = [
-            {"index": 0, "column": expected_cols[0], "value": "some text 1", "display_value": "some text 1"},
-            {"index": 1, "column": expected_cols[0], "value": "some text 2", "display_value": "some text 2"},
+            {"row_index": 0, "column": expected_cols[0], "value": "some text 1", "display_value": "some text 1"},
+            {"row_index": 1, "column": expected_cols[0], "value": "some text 2", "display_value": "some text 2"},
         ]
         assert tab.rows[0] == expected_rows[0]
         assert tab.rows[1] == expected_rows[1]
