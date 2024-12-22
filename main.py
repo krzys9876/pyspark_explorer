b165d078a7da5928fb82e8c0fc39a900c617a292
Krzysztof Ruta <krzysztof.ruta@play.pl>
2024-12-22 14:32:35 +0100

    improve spark logging (redirect to file), ensure spark local ip to avoid problems with restrictive local firewalls

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import os
 import sys
-
 from pyspark.sql import SparkSession
 
 import pyspark_explorer.ui as ui
@@ -7,7 +7,12 @@ from pyspark_explorer.explorer import Explorer
 
 
 def main() -> None:
-    spark = SparkSession.builder.master("local[2]").appName("pyspark_test01").getOrCreate()
+    spark = (SparkSession.builder
+             .master("local[2]")
+             # redirect all logs to file
+             .config("spark.driver.extraJavaOptions", "-Dlog4j.configuration=file:log4j.properties")
+             .appName("pyspark_explorer")
+             .getOrCreate())
 
     explorer = Explorer(spark)
     app = ui.DataApp(explorer, sys.argv[1])
@@ -17,4 +22,5 @@ def main() -> None:
 
 
 if __name__ == "__main__":
+    os.environ["SPARK_LOCAL_IP"] = "127.0.0.1"
     main()
