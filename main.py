6525b6d219502ddaf70069cf54516c5c45776594
Krzysztof Ruta <krzysztof.ruta@play.pl>
2024-12-30 23:45:56 +0100

    provide entry point (pyspark-explorer), enable logging if log4j.properties file is provided

--- main.py
+++ main.py
@@ -1,26 +1,5 @@
-import os
-import sys
-from pyspark.sql import SparkSession
-
-import pyspark_explorer.ui as ui
-from pyspark_explorer.explorer import Explorer
-
-
-def main() -> None:
-    spark = (SparkSession.builder
-             .master("local[2]")
-             #.config("spark.driver.extraJavaOptions", "-Dlog4j.shutdownHookEnabled=false")
-             .config("spark.log.level", "FATAL")
-             .appName("pyspark_explorer")
-             .getOrCreate())
-
-    explorer = Explorer(spark)
-    app = ui.DataApp(explorer, sys.argv[1] if len(sys.argv)>1 else "/")
-    app.run()
-
-    spark.stop()
+from pyspark_explorer.run import run
 
 
 if __name__ == "__main__":
-    os.environ["SPARK_LOCAL_IP"] = "127.0.0.1"
-    main()
+    run()
